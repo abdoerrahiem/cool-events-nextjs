b3e08d0424a6f0158eeeef0a426dbf7cb51ff51b
Abdur Rahim <abdoerrahiem@gmail.com>
2021-04-30 02:19:32 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Tentang</h1>
+      <p>
+        Aplikasi yang dapat kamu gunakan untuk mencari acara-acara yang keren
+        dan menarik
+      </p>
+      <p>Versi: 1.0.0</p>
+    </div>
+  )
+}
